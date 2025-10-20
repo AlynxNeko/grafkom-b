3245eb4b3e9e3ef2b084f68bff10044696b996de
AlynxNeko <109527959+AlynxNeko@users.noreply.github.com>
2025-10-20 16:54:32 +0700

    finished js

--- pertemuan-05-instanced-arbitrary-hierarchical/libs.js
+++ pertemuan-05-instanced-arbitrary-hierarchical/libs.js
@@ -93,5 +93,40 @@ var LIBS = {
 
     set_position: function (m, x, y, z) {
         m[12] = x, m[13] = y, m[14] = z;
+    },
+
+    multiply: function (m1, m2) {
+        var rm = this.get_I4();
+        var N = 4;
+        for (var i = 0; i < N; i++) {
+            for (var j = 0; j < N; j++) {
+                rm[i * N + j] = 0;
+                for (var k = 0; k < N; k++) {
+                    rm[i * N + j] += m1[i * N + k] * m2[k * N + j];
+                }
+            }
+        }
+        return rm;
+    },
+    scaleX: function (m, t) {
+        m[0] *= t;
+    },
+    scaleY: function (m, t) {
+        m[5] *= t;
+    },
+    scaleZ: function (m, t) {
+        m[10] *= t;
+    },
+    multiply: function (m1, m2) {
+        var rm = this.get_I4();
+        var N = 4;
+        for (var i = 0; i < N; i++) {
+            for (var j = 0; j < N; j++) {
+                rm[i * N + j] = 0;
+                for (var k = 0; k < N; k++)
+                    rm[i * N + j] += m1[i * N + k] * m2[k * N + j];
+            }
+        }
+        return rm;
     }
 };
